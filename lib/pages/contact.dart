9f6dc21af446099d3537e0622f40bde816634fba
James Jongyoon Han <jameshan@Jamess-MacBook-Pro.local>
2020-07-18 13:27:07 -0700

    adding

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Contact extends StatefulWidget {
   @override
@@ -10,7 +11,52 @@ class _ContactState extends State<Contact> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Center(child:Container(
+        width: 300,
+        height: 300,
+        child: MaterialButton(
+          shape: CircleBorder(side: BorderSide(width: 15, color: Colors.greenAccent, style: BorderStyle.solid)),
+          child: Icon(Icons.phone, size: 50,),
+          color: Colors.green,
+          textColor: Colors.white,
+          onPressed: () async{
+            showDialog(context: context,
+                barrierDismissible: true,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Are You Sure You Want to Activate Emergency?'),
+                    actions: <Widget>[
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Divider(),
+                          RaisedButton(
+                            child: Text('Cancel'),
+                            elevation: 12,
+                            onPressed: (){
+                              Navigator.pop(context);
+                            },
+                          ),
+                          SizedBox(width: 10,),
+                          RaisedButton(
+                            child: Icon(Icons.volume_up),
+                            color: Colors.redAccent,
+                            elevation: 12,
+                            onPressed: () async{
+                              launch("tel://3107559222}");
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  );
+                }
+            );
+
+          },
+        ),
+      ),
+    ),
     );
   }
 }
--- lib/pages/graphs.dart
+++ lib/pages/graphs.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hisi/classes/line_chart_hisi.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class Graphs extends StatefulWidget {
   @override
@@ -9,8 +11,78 @@ class Graphs extends StatefulWidget {
 class _GraphsState extends State<Graphs> {
   @override
   Widget build(BuildContext context) {
+    List<charts.Series<LinearSales, int>> _createSampleData() {
+      final data = [
+        new LinearSales(0, 6),
+        new LinearSales(1, 25),
+        new LinearSales(2, 100),
+        new LinearSales(3, 75),
+      ];
+
+      return [
+        new charts.Series<LinearSales, int>(
+          id: 'Sales',
+          colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+          domainFn: (LinearSales sales, _) => sales.year,
+          measureFn: (LinearSales sales, _) => sales.sales,
+          data: data,
+        )
+      ];
+    }
+
     return Scaffold(
-      body: Container(),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text('Package ', style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold
+                      ),),
+                      Text('100010', style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold
+                      ))
+                    ],
+                  ),
+                  Divider(thickness: 3,),
+                  Text('Type: Medicine')
+                ],
+              ),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(25))
+            ),),
+            Container(
+              width: 150,
+              height: 150,
+              child: SimpleLineChart(_createSampleData(), animate: true,),
+            )
+          ],
+        ),
+      ),
     );
   }
+}
+
+class LinearSales {
+  final int year;
+  final int sales;
+
+  LinearSales(this.year, this.sales);
 }
